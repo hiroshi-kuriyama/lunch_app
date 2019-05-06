e1d45742a1813e7ab672f47cf9105c6be203094f
Hiroshi Kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2019-05-07 08:45:36 +0900

    20190506

--- .ipynb_checkpoints/selector-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import beautifulsoup4
-
-url = "https://tabelog.com/tokyo/"
-
-print(request(url))
-request.
--- genre_scraper.py
+++ genre_scraper.py
@@ -15,32 +15,4 @@ for e in elems:
         links.append(e.find('a').get('href'))
 genre_link_dict = dict(zip(genres, links))
 with open('./static/genres_data/tokyo_genres.json', mode='w') as f:
-    json.dump(genre_link_dict, f)
-
-def extarct_genre_links(url):
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-
-    elems = soup.find_all('li', class_="areatop-genre-rank__genre-item")
-    genres = []
-    links = []
-    for e in elems:
-        if e.find('a') != None:
-            genres.append(e.getText())
-            links.append(e.find('a').get('href'))
-    return dict(zip(genres, links))
-
-def memo():
-    """
-    ほしい関数。
-    ページ内のジャンル数の数を数えてくれる関数
-    ジャンルの番号を指定すればそのジャンル名とそのリンク先のランキング上位の画像のリンクを返してくれる関数
-    """
-
-def random_ganre(url):
-    """
-    取得したURLのジャンルからランダムに９個のジャンルを選択する。
-    その先の画像リンク先とジャンル名を返す
-    """
-    genre_dict = extarct_genre_links(url)
-    genre_dict_rand = random.choice(genre_dict)
+        json.dump(genre_link_dict, f)
