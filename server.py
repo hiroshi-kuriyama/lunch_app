96d3e078da8cec0ccafe2bbca15ab3e871b93076
Hiroshi Kuriyama <29877317+hiroshi-kuriyama@users.noreply.github.com>
2019-05-25 01:00:24 +0900

    20190524

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 import os
 import random
+from modules import choice_shop
 
 app = Flask(__name__)
 
@@ -9,8 +10,17 @@ app = Flask(__name__)
 def index():
     img_file_names = os.listdir('./static/img/')
     img_path_0 = "/static/img/" + random.choice(img_file_names)
-    test_str = 'aiueo'
-    return render_template('index.html', img_path_0=img_path_0, test_str=img_file_names)
+    url = 'https://tabelog.com/tokyo/A1304/A130401/R7443/'
+    genre_dict = choice_shop.get_genre_dict(url)
+    genre_url, genre_nm = random.choice(list(genre_dict.items()))
+    shop_dcit = choice_shop.get_shops_dict(genre_url)
+    shop_url, shop_elem_dict = random.choice(list(shop_dcit.items()))
+    return render_template('index.html', 
+    img_path_0=img_path_0, 
+    test_str=img_file_names,
+    nm=shop_elem_dict['nm'],
+    img_url=shop_elem_dict['img_url'],
+    url=shop_url)
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=9000)
